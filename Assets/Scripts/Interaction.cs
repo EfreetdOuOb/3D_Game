6886188417820378619b376ba676a91173727a44
EfreetdOuOb <efreetdouob@gmail.com>
2025-12-01 18:11:50 +0800

    調整角色碰撞箱

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -5,6 +5,8 @@ public class Interaction : MonoBehaviour
     public GameObject KeyInfo;
     public GameObject TextPanel;
 
+    public bool isPressedE = false;
+
     void Awake()
     {
         KeyInfo.SetActive(false);
@@ -14,10 +16,16 @@ public class Interaction : MonoBehaviour
 
     void Update()
     {
-        if(KeyInfo.activeSelf && Input.GetKeyDown(KeyCode.E))
-        {
-            TextPanel.SetActive(true);
-        }
+         if((KeyInfo.activeSelf || isPressedE) && Input.GetKeyDown(KeyCode.E))
+    {
+        // 切換狀態
+        isPressedE = !isPressedE;
+        TextPanel.SetActive(isPressedE);
+        
+        // 如果需要在打開視窗時隱藏提示，可以加這行：
+        // KeyInfo.SetActive(!isPressedE); 
+    }
+        
     }
 
     public void OnTriggerEnter(Collider other)
